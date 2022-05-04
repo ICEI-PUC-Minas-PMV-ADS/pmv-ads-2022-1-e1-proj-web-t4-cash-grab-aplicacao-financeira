16d2d58d9de82b1e426c71c7ffa4bcdaef052c68
Vitor Hugo <vitorhugoemail@gmail.com>
2022-05-04 07:22:00 -0300

    Mudado a posição dos campos que adicionam o valor da divida e do limite e arrumado o bug da funcionalidade

--- script/despesas.js
+++ script/despesas.js
@@ -2,15 +2,16 @@
 let limiteProposto = document.getElementById('limiteproposto');
 let btnAdcionarLimite = document.getElementById('adiconarLimite');
 let barraProgesso = document.getElementById('visualizador');
+let gastoInput=document.getElementById('gastos');
+let somaDividas= document.getElementById('somaDividas');
+let btncolocarDividas=document.getElementById('colocarDividas')
 let porcetagemDivida;
+let valorLimiteEl = document.getElementById('valorLimite');
 
 /*Função para modificar a barra de progresso*/
 function limiteUser(){
-    let valorLimiteEl = document.getElementById('valorLimite');
-    let gastoInput=document.getElementById('gastos');
-    console.log(porcetagemDivida)
-    valorLimiteEl.innerHTML= limiteProposto.value;
-    if(limiteProposto.value==gastoInput.value){
+    
+    if(valorLimiteEl.value>=somaDividas.value){
         barraProgesso.style.width="100%";
         barraProgesso.style.background='red'
         barraProgesso.innerHTML='100%';
@@ -18,7 +19,6 @@ function limiteUser(){
     else{
       porcetagemDivida= (gastoInput.value*1)/limiteProposto.value; 
       porcetagemDivida = Math.round(porcetagemDivida*100);
-      console.log(porcetagemDivida)
       barraProgesso.style.width=porcetagemDivida+'%';
       barraProgesso.innerHTML=porcetagemDivida+'%';
       if(porcetagemDivida==60){
@@ -28,6 +28,17 @@ function limiteUser(){
       }
     }
 }
-
+function colocarLimite(){
+    valorLimiteEl.value=limiteProposto.value;
+    valorLimiteEl.innerHTML= limiteProposto.value;
+    limiteUser()       
+}
+function colocarDividas(){
+    somaDividas.value=gastoInput.value
+    somaDividas.innerHTML=gastoInput.value
+    limiteUser()
+    
+}
 /*Chamando a função atraves do click no botão*/
-btnAdcionarLimite.addEventListener('click',limiteUser);
+btnAdcionarLimite.addEventListener('click',colocarLimite);
+btncolocarDividas.addEventListener('click',colocarDividas);

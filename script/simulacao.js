594b3d46a3f786533d670225a53cd2e04b489d34
mthszf99 <matheus.pires@sga.pucminas.br>
2022-05-13 16:00:36 -0300

    atualizacoes de alguns elementos

--- script/simulacao.js
+++ script/simulacao.js
@@ -8,4 +8,5 @@ function capturar() {
     juros = 2/100;
     document.getElementById("parcelas").innerHTML = (valor*Math.pow((1+juros),meses)/meses).toFixed(2);
     document.getElementById("montante").innerHTML = (valor*Math.pow((1+juros),meses)).toFixed(2);
+    document.getElementById("jurosemp").innerHTML = (valor*Math.pow((1+juros),meses)-(valor)).toFixed(2);
 }

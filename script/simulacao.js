735efb06e0e277b9ea52ba7117b083f532dad51c
mthszf99 <matheus.pires@sga.pucminas.br>
2022-05-20 16:19:32 -0300

    comecando a fazer a funcionalidade de investimentos

--- script/simulacao.js
+++ script/simulacao.js
@@ -5,8 +5,24 @@ var meses = "";
 function capturar() {
     valor = document.getElementById("valorEmprestimo").value;
     meses = document.getElementById("mesesEmprestimo").value;
-    juros = 2/100;
+    juros = 1/100;
     document.getElementById("parcelas").innerHTML = (valor*Math.pow((1+juros),meses)/meses).toFixed(2);
-    document.getElementById("montante").innerHTML = (valor*Math.pow((1+juros),meses)).toFixed(2);
     document.getElementById("jurosemp").innerHTML = (valor*Math.pow((1+juros),meses)-(valor)).toFixed(2);
+    document.getElementById("montante").innerHTML = (valor*Math.pow((1+juros),meses)).toFixed(2);
+}
+
+/*calculo do investimento*/
+/*consertar retorno mensal e retorno total*/
+var valorinv = "";
+var mesesinv = "";
+var aporte = "";
+
+function captura() {
+    valorinv = document.getElementById("valorInvestimento").value;
+    mesesinv = document.getElementById("mesesInvestimento").value;
+    aporte = document.getElementById("aporteMensal").value;
+    jurosinv = 1/100;
+    document.getElementById("retornoInicial").innerHTML = (valorinv*Math.pow((1+jurosinv),mesesinv)).toFixed(2);
+    document.getElementById("retornoMensal").innerHTML = (aporte*Math.pow((1+jurosinv),mesesinv-1)/(jurosinv)).toFixed(2);
+    document.getElementById("retornoTotal").innerHTML = (valorinv*Math.pow((1+jurosinv),mesesinv)).toFixed(2);
 }

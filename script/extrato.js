ac94582701be01a635a66f0d243332f4ac4b863f
Pedro Almeida <pedroassis1707@gmail.com>
2022-06-21 21:58:38 -0300

    responsividade celular

--- script/extrato.js
+++ script/extrato.js
@@ -90,6 +90,32 @@ if(adicionar == true)
 }
 } );
 
+/*Parte onde faz o menu no celular aparecer*/
+let menuCell = document.getElementsByClassName('imgMenuCell');
+
+function aparecerMenu(){
+  let menu = document.getElementById('teste');
+  menu.classList.toggle('visibilidadeMenuCell');
+  menu.classList.toggle('menu');
+  let textoInicio = document.getElementById('textoInicio')
+  let textoDespesas = document.getElementById('textoDespesas')
+  let textoExtrato= document.getElementById('textoExtrato')
+  let textoSimulacao = document.getElementById('textoSimulacao')
+  let textoNoticias = document.getElementById('textoNoticias')
+  let textoSobre = document.getElementById('textoSobre')
+  textoInicio.classList.toggle('textoInicio');
+  textoInicio.classList.toggle('textoMenucelular');
+  textoDespesas.classList.toggle('textoMenucelular');
+  textoDespesas.classList.toggle('textoDespesas');
+  textoExtrato.classList.toggle('textoMenucelular');
+  textoExtrato.classList.toggle('textoExtrato');
+  textoSimulacao.classList.toggle('textoMenucelular');
+  textoSimulacao.classList.toggle('textoSimulacao');
+  textoNoticias.classList.toggle('textoMenucelular');
+  textoNoticias.classList.toggle('textoNoticias');
+  textoSobre.classList.toggle('textoMenucelular');
+  textoSobre.classList.toggle('textoSobre');
+}
 
   /*alert inicial*/
 alert("Prezado (a), Seja bem-vindo ao seu extrato Báncario! 🙃😁😜");
